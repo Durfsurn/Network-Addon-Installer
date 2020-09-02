a6ea3110e1e6788ca4b4cb4eed8d04e6afb960dd
durfsurn <>
2020-09-02 21:45:43 +1000

    updated pipeline deps

--- elm_compile.sh
+++ elm_compile.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-cd elm
-elm make src/Main.elm --output=../static/main.js
+docker build --tag elm-builder elm
+docker run --rm -d --name elmb elm-builder 
+docker cp $PWD/elm/src elmb:/workdir
+docker exec -i elmb /elm/elm make --output=src/main.js src/Main.elm
+docker cp elmb:/workdir/src/main.js $PWD/static
+docker rm -f elmb
+
+if [ ! -f "$PWD/static/main.js" ];then
+exit 125
+fi
